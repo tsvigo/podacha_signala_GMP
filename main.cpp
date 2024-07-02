9e8782c1f3aea32ddfd91062582cf8226720aef3
Viktor Tsvigo <11_70@bk.ru>
2024-07-02 23:21:02 +0300

    Описание коммита

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// #include <QCoreApplication>
+ #include <QApplication>
 
 // int main(int argc, char *argv[])
 // {
@@ -14,11 +14,16 @@
 #include <gmpxx.h>
 #include <iostream>
 #include <vector>
-
-const QString BMP_FILE_NAME = "image.bmp"; // название надо брать из диалога выбора файла
+#include <QFileDialog>
+//const
+    QString BMP_FILE_NAME = "image.bmp"; // название надо брать из диалога выбора файла
 const QString RANDOM_NUMBERS_FILE_NAME = "/home/viktor/my_projects_qt_2/sgenerirovaty_neyroni_GMP/random_numbers.bin";
-const QString RESULT_FILE_NAME = "result_vector.bin";
-
+//const
+//QString RESULT_FILE_NAME = "result_vector.bin"; // результат склеиваем
+//const
+//  QString    RESULT_FILE_NAME=BMP_FILE_NAME+"_result_vector.bin";
+// /home/viktor/1_rukoy_GMP/Sprite-0001-m.bmp
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 std::vector<mpz_class> readBMPFile(const QString& fileName) {
     QImage image(fileName);
     if (image.isNull()) {
@@ -70,14 +75,33 @@ void writeResultToFile(const QString& fileName, const std::vector<mpz_class>& nu
 }
 
 void printVector(const std::vector<mpz_class>& vec) {
+    int i=0;
     for (const auto& number : vec) {
-        std::cout << number.get_str() << std::endl;
+        std::cout<<i<<":" << number.get_str() << std::endl;
+        i++;
     }
 }
 
 int main(int argc, char *argv[]) {
-    QCoreApplication a(argc, argv);
+  //  QCoreApplication a(argc, argv);
+    // Используем QApplication вместо QCoreApplication, чтобы поддерживать графические компоненты
+    QApplication app(argc, argv);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Вызов диалога выбора файла
+    BMP_FILE_NAME = QFileDialog::getOpenFileName(nullptr, "Выберите файл",
+                                    "/home/viktor/1_rukoy_GMP/"
+                                                            ,  "bmp Files (*.bmp)");
 
+    QString
+        RESULT_FILE_NAME=BMP_FILE_NAME+"_result_vector.bin";
+
+    // Проверка, был ли файл выбран
+    if (!BMP_FILE_NAME.isEmpty()) {
+        qDebug() << "Выбранный файл:" << BMP_FILE_NAME;
+    } else {
+        qDebug() << "Файл не был выбран.";
+    }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Чтение BMP файла и преобразование в вектор mpz_class
     std::vector<mpz_class> bmpVector = readBMPFile(BMP_FILE_NAME);
 
@@ -87,7 +111,9 @@ int main(int argc, char *argv[]) {
     // Формирование общего вектора
     std::vector<mpz_class> resultVector;
     resultVector.insert(resultVector.end(), bmpVector.begin(), bmpVector.begin() + 100);
-    resultVector.insert(resultVector.end(), randomVector.begin(), randomVector.end());
+    resultVector.insert(resultVector.end(), randomVector.begin(),randomVector.begin() + 105
+                  //      randomVector.end()
+                        );
 
     // Вывод результирующего вектора в консоль
     printVector(resultVector);
@@ -95,6 +121,8 @@ int main(int argc, char *argv[]) {
     // Запись результирующего вектора в бинарный файл
     writeResultToFile(RESULT_FILE_NAME, resultVector);
 
-    return a.exec();
+    qDebug() << "RESULT_FILE_NAME: "<<RESULT_FILE_NAME;
+    qDebug() <<"resultVector.size()="<<resultVector.size();
+    return app.exec();
 }
 
